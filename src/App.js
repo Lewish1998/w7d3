2e57521f7ff2b9c6fbc77ba59a6d2825a228f9ae
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-02-24 15:25:05 +0000

    x

--- src/App.js
+++ src/App.js
@@ -1,14 +1,1 @@
-import './App.css';
-import PokemonContainer from './containers/PokemonContainer';
-
-function App() {
-  return (
-    <div>
-      <h1 id='heading'>Pokemon</h1>
-      <hr></hr>
-      <PokemonContainer/>
-    </div>
-  );
-}
-
-export default App;
+../components/PokemonList
